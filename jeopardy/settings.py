5a6757d336d4c856274c60e196a47fee5b7fc137
Karthik Ramachandran <karthik.ramachandran@duke.edu>
2019-11-02 17:35:10 -0400

    added postgres db for heroku

--- jeopardy/settings.py
+++ jeopardy/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'jeopardy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'jeopardy_db',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
