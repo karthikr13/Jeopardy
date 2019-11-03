531709c9bd98c29f772998b12c6f7eaef6a94c85
Karthik Ramachandran <karthik.ramachandran@duke.edu>
2019-11-02 20:24:32 -0400

    debug False, sensitive info removed

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Jeopardy board game using jservice.io API to collect questions.
 
 Hosted on heroku at https://karthik-jeopardy.herokuapp.com/.
 Built using Django on Python, front-end using HTML, CSS, and Javascript.
+
+karthikr1204@gmail.com
--- jeopardy/settings.py
+++ jeopardy/settings.py
@@ -13,21 +13,17 @@ import os
 import dj_database_url
 import django_heroku
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "#!u-_p@kpc@lz%-xpjr%trpk$_ekda*ticcgj7if0#&-2oktjs"
+#sensitive information has been removed from this file in version control
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = "SECRET_KEY"
 
-# Application definition
+DEBUG = False
+
 
 INSTALLED_APPS = [
     'board.apps.BoardConfig',
@@ -36,9 +32,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # Disable Django's own staticfiles handling in favour of WhiteNoise, for
-    # greater consistency between gunicorn and `./manage.py runserver`. See:
-    # http://whitenoise.evans.io/en/stable/django.html#using-whitenoise-in-development
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
 ]
@@ -75,20 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'jeopardy.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'jeopardy_db',
-        'USER': 'django',
-        'PASSWORD': 'django',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
 DATABASES['default'].update(dj_database_url.config(conn_max_age=500, ssl_require=True))
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -105,35 +84,23 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Internationalization
-# https://docs.djangoproject.com/en/2.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# Allow all host headers
 ALLOWED_HOSTS = ['*']
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, 'static'),
 ]
 
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Activate Django-Heroku.
 django_heroku.settings(locals())
